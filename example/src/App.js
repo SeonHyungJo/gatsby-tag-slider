47ba787dc8c1e251bdc1c889fb2d0ca3cb61b4be
SeonHyungJo <seonhyung.jo@gmail.com>
2019-12-07 00:43:40 +0900

    Release 1.1.1 / Add Color & Click Event

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,81 @@
 import React, { Component } from 'react'
 import TagSlider from 'gatsby-tag-slider'
 
-const tags = ["react", "network", "es3", "react2", "network2", "es32", "react3", "network3", "es33"]
+const tags = {
+  'react': {
+    id: 'react',
+    name: 'react',
+    selected: false
+  },
+  'network': {
+    id: 'network',
+    name: 'network',
+    selected: false
+  },
+  'es3': {
+    id: 'es3',
+    name: 'es3',
+    selected: false
+  },
+  'java': {
+    id: 'java',
+    name: 'java',
+    selected: false
+  },
+  'sprint': {
+    id: 'sprint',
+    name: 'sprint',
+    selected: false
+  },
+  'vue': {
+    id: 'vue',
+    name: 'vue',
+    selected: false
+  },
+  'js': {
+    id: 'js',
+    name: 'js',
+    selected: false
+  },
+  'es5': {
+    id: 'es5',
+    name: 'es5',
+    selected: false
+  },
+  'tdd': {
+    id: 'tdd',
+    name: 'tdd',
+    selected: false
+  }
+}
 
 export default class App extends Component {
-  render () {
+  constructor() {
+    super()
+    this.state = {
+      tags
+    }
+  }
+
+  selectHandle = (id) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        'tags': {
+          ...prevState.tags,
+          [id]: {
+            ...prevState.tags[id],
+            selected: !prevState.tags[id].selected
+          }
+        }
+      }
+    })
+  }
+
+  render() {
     return (
       <div>
-        <TagSlider tags = {tags}  />
+        <TagSlider tags={this.state.tags} selectHandle={this.selectHandle} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.css'
 
-const TagSlider = ({ tags, addTag }) => {
-  // const { movePos, setMovePos } = setState({})
+const TagSlider = ({ tags, selectHandle, addTag }) => {
   const [moveFlag, setMoveFlag] = useState(false)
+  const [canClick, setCanClick] = useState(true)
   const [startPos, setstartPos] = useState(0)
   const sliderEl = useRef(null)
 
@@ -18,13 +18,17 @@ const TagSlider = ({ tags, addTag }) => {
   const mouseMoveHandle = (e) => {
     if (moveFlag) {
       const clientX = e.clientX || e.touches[0].clientX
-      sliderEl.current.scrollTo(sliderEl.current.scrollLeft + startPos - clientX, 0)
+      const gap = startPos - clientX
+
+      gap < 2 && setCanClick(false)
+      sliderEl.current.scrollTo(sliderEl.current.scrollLeft + gap, 0)
       setstartPos(clientX)
     }
   }
 
   const mouseUpHandle = (e) => {
     setMoveFlag(false)
+    e.preventDefault()
   }
 
   return (
@@ -40,9 +44,13 @@ const TagSlider = ({ tags, addTag }) => {
         onTouchEnd={mouseUpHandle}
       >
         {
-          tags.map((tag) =>
-            <div key={`tag-slider-${tag}`} className={`${styles.tagItem} ${styles.noselect}`}>
-              <span className={styles.tagName}>{tag}</span>
+          Object.values(tags).map(({ id, name, selected }) =>
+            <div
+              key={`tag-slider-${id}`}
+              className={`${styles.tagItem} ${selected ? styles.tagSeleted : ''} ${styles.noselect}`}
+              onClick={() => canClick ? selectHandle(id) : setCanClick(true)}
+            >
+              <span className={styles.tagName}>{name}</span>
             </div>
           )
         }
@@ -52,7 +60,12 @@ const TagSlider = ({ tags, addTag }) => {
 }
 
 TagSlider.propTypes = {
-  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tags: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    selected: PropTypes.bool
+  }),
+  selectHandle: PropTypes.func,
   addTag: PropTypes.func
 }
 
